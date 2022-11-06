dcb78afc8b951474e4ed8b0a3a2c0626904e65bd
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2022-11-06 12:16:21 -0300

    Update codigo-2.py

--- codigo-2.py
+++ codigo-2.py
@@ -4,7 +4,8 @@ conn = http.client.HTTPSConnection("receitaws.com.br")
 
 headers = { 'Content-Type': "application/json" }
 
-conn.request("GET", "/v1/cnpj/33291488000102", headers=headers)
+# conn.request("GET", "/v1/cnpj/33291488000102", headers=headers)
+conn.request("GET", "/v1/cnpj/17162280000137", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
